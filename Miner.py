80e3b1fe5a324ad657114502ef34d3badc82d912
Marco-make <tudelanomarco@gmail.com>
2024-08-04 19:00:34 -0500

    Create Miner.py

---
+++ Miner.py
@@ -0,0 +1,11 @@
+# Clonar el repositorio
+!git clone https://github.com/revoxhere/duino-coin
+
+# Cambiar al directorio del repositorio
+%cd duino-coin
+
+# Instalar las dependencias necesarias
+!pip install -r requirements.txt
+
+# Ejecutar el script PC_Miner.py
+!python PC_Miner.py
